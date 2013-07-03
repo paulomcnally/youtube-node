fdf35d6608aafe929717dca2e6234053d9520af0
paulomcnally <paulomcnally@gmail.com>
2013-07-03 08:11:16 -0600

    0.0.1

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var youtube = require('../lib/youtube');
+
+
+youtube.search('World War z Trailer', 2, function(resultData) {
+    console.log(resultData);
+});
+
+
+youtube.getById('HcwTxRuq-uk', function(resultData) {
+    console.log(resultData);
+});
