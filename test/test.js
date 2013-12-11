3c1b9ebd1c542dacbddd4c8146bec1be2a5ed48e
Paulo McNally <paulomcnally@gmail.com>
2013-12-10 21:22:35 -0600

    Fix empty data.feed.entry

--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
 var youtube = require('../lib/youtube');
 
-
+/*
 youtube.search('World War z Trailer', 2, function(resultData) {
     console.log(resultData);
 });
+*/
 
 
 youtube.getById('HcwTxRuq-uk', function(resultData) {
