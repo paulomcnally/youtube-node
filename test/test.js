5406240a00badece739bbf43f191d3a81bba753d
Paulo McNally <paulomcnally@gmail.com>
2014-03-02 14:58:46 -0600

    fix response on getById

--- test/test.js
+++ test/test.js
@@ -3,10 +3,14 @@ var youtube = require('../lib/youtube');
 
 youtube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU');
 
-youtube.getById('HcwTxRuq-uk', function(resultData) {
+youtube.getById('HcwTxRuq-uk,vIu85WQTPRc', function(resultData) {
     console.log( JSON.stringify( resultData, null, 2 ) );
 });
+
+/*
 
 youtube.search('World War z Trailer', 2, function(resultData) {
     console.log( JSON.stringify( resultData, null, 2 ) );
 });
+
+    */
