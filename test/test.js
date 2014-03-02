fa68816e2ef4cec2c42d659d589bbea2235be1c5
Paulo McNally <paulomcnally@gmail.com>
2014-03-02 14:59:23 -0600

    added two IDs on getById

--- test/test.js
+++ test/test.js
@@ -7,10 +7,6 @@ youtube.getById('HcwTxRuq-uk,vIu85WQTPRc', function(resultData) {
     console.log( JSON.stringify( resultData, null, 2 ) );
 });
 
-/*
-
 youtube.search('World War z Trailer', 2, function(resultData) {
     console.log( JSON.stringify( resultData, null, 2 ) );
 });
-
-    */
