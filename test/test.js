44b35fe55955293203bd8546a5efc59751af9443
Paulo McNally <paulomcnally@gmail.com>
2014-12-06 17:34:00 -0600

    best practices on code style

--- test/test.js
+++ test/test.js
@@ -1,12 +1,11 @@
 var youtube = require('../lib/youtube');
 
-
 youtube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU');
 
 youtube.getById('HcwTxRuq-uk,vIu85WQTPRc', function(resultData) {
-    console.log( JSON.stringify( resultData, null, 2 ) );
+  console.log(JSON.stringify(resultData, null, 2));
 });
 
 youtube.search('World War z Trailer', 2, function(resultData) {
-    console.log( JSON.stringify( resultData, null, 2 ) );
+  console.log(JSON.stringify(resultData, null, 2));
 });
